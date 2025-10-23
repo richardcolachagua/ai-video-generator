47a766865a910ac0c46fa261636ac152241363b1
Richard Colachagua <richardcolachagua@gmail.com>
2025-10-23 16:02:58 -0400

    Created homepage file and moved files to new folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
-import HowItWorks from "./components/HowItWorks";
-import GalleryPreview from "./components/GalleryPreview";
-import Footer from "./components/Footer";
-
-// Placeholder pages
-function HelpPage() {
-  return <div className="text-white p-8">Help/FAQ coming soon.</div>;
-}
-function TermsPage() {
-  return <div className="text-white p-8">Terms & Privacy coming soon.</div>;
-}
-function CreatePage() {
-  return <div className="text-white p-8">Create page coming soon.</div>;
-}
-function GalleryPage() {
-  return <div className="text-white p-8">Gallery page coming soon.</div>;
-}
+import Homepage from "./pages/Homepage";
 
 export default function App() {
   return (
     <Router>
       <div className="bg-ironmanBlack min-h-screen flex flex-col">
-        <Navbar />
         <main className="pt-24 flex-1">
           <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <HeroSection />
-                  <HowItWorks />
-                  <GalleryPreview />
-                </>
-              }
-            />
-            <Route path="/help" element={<HelpPage />} />
-            <Route path="/terms" element={<TermsPage />} />
-            <Route path="/create" element={<CreatePage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
+            <Route path="/homepage" element={<Homepage />} />
           </Routes>
         </main>
-        <Footer />
       </div>
     </Router>
   );
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "../components/HeroSection";
+import HowItWorks from "../components/HowItWorks";
+import GalleryPreview from "../components/GalleryPreview";
+
+export default function Homepage() {
+  return (
+    <>
+      <HeroSection />
+      <HowItWorks />
+      <GalleryPreview />
+    </>
+  );
+}
